735b0d764b897e6c133b9bdfa6edd46a36a27b3d
Juan Francisco Chango <jfchangop@falabella.cl>
2024-07-11 22:47:33 -0500

    reactive

--- src/components/CatComposition.vue
+++ src/components/CatComposition.vue
@@ -1,19 +1,41 @@
 <script setup>
-import {ref} from "vue";
+import {reactive, ref} from "vue";
 
+const title = ref("CatComposition");
 const counter = ref(0)
+
+const counterData = reactive({
+  title: "CatComposition Two",
+  count: 10,
+})
+
 const increaseCount = () => {
   counter.value++
+  counterData.count++
 }
 </script>
 
 <template>
   <div class="main">
+    <h1>{{title}}</h1>
     <h1>This is a cat counter</h1>
 
     <h2 class="counter">{{ counter }}</h2>
 
     <button @click="increaseCount">Add a cat</button>
+
+    <input v-model="title" type="text">
+
+
+    <h1>Object Reactive</h1>
+    <h1>{{counterData.title}}</h1>
+
+
+    <h2 class="counter">{{ counterData.count }}</h2>
+
+    <button @click="increaseCount">Add a cat</button>
+
+    <input v-model="counterData.title" type="text">
 
   </div>
 </template>
